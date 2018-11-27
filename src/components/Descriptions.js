9b16642c3cf807e6d340b2fc6ba5f38e0dfc2691
unknown <sonkengmboukengherv@yahoo.fr>
2018-11-27 16:22:50 +0100

    first commit

---
+++ src/components/Descriptions.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Descriptions extends Component
+{
+render()
+{
+    return(
+        <div className = " description">
+            <h5>Hour Minute Seconde</h5>
+        </div>
+    )
+}
+}
+export default Descriptions;
---
+++ src/components/Hour.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Hour extends Component
+{
+render()
+{
+    return(
+        <div className = 'time'>
+            <h1>00 : 30 : 00</h1>
+        </div>
+    );
+}
+}
+export default Hour;
